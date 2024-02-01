c2c28019ed265171007017ad0f40c5811ce33886
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-01 22:28:12 +0100

    fix: dettagli comic

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,16 +1,37 @@
 @extends('layout.app')
 
 @section('main')
-    <figure class="figure">
-        <img src="{{ $comic->thumb }}" class="rounded" alt="{{ $comic->title }}">
-    </figure>
     <div>
         <h3 class="my-3">{{ $comic->title }}</h3>
-        <p class="mb-3">{{ $comic->description }}</p>
-        <div>Serie: {{ $comic->series }}</div>
-        <div>Price: {{ $comic->price }}$</div>
-        <div>Sale date: {{ $comic->sale_date }}</div>
-        <div>Type: {{ $comic->type }}</div>
+        <figure class="figure">
+            <img src="{{ $comic->thumb }}" class="rounded" alt="{{ $comic->title }}">
+        </figure>
+        <h5>Description</h5>
+        <p class="my-4">{{ $comic->description }}</p>
+        <div>
+            <ul class="d-flex gap-5 list-unstyled">
+                <li class="mb-3">
+                    <h5>Serie</h5> {{ $comic->series }}
+                </li>
+                <li class="mb-3">
+                    <h5>Price</h5>{{ $comic->price }}$
+                </li>
+                <li class="mb-3">
+                    <h5>Sale date</h5> {{ $comic->sale_date }}
+                </li>
+                <li class="mb-3">
+                    <h5>Type</h5> {{ $comic->type }}
+                </li>
+            </ul>
+            <div>
+                <h5>Artists</h5>
+                <ul class="list-unstyled">
+                    @foreach ($comic->artists as $artist)
+                        <li>{{ $artist }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
     </div>
     <a href="{{ route('comics.index') }}">Back</a>
 @endsection
