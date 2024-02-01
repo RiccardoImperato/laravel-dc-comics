299ff29425de3b570d506a8c0e5dec1ff2e15ef7
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-01 22:26:06 +0100

    fix: link home e add comic

--- resources/views/comics/list.blade.php
+++ resources/views/comics/list.blade.php
@@ -2,12 +2,12 @@
 
 @section('main')
     <h1>Comics</h1>
-    <a href="{{ route('comics.create') }}">Add comic</a>
+    <a href="{{ route('home') }}">Home</a> - <a href="{{ route('comics.create') }}">Add comic</a>
     <ul class="list-unstyled mt-3">
         @foreach ($comics as $comic)
-            <li>{{ $comic->id }} - {{ $comic->title }} - <a href="{{ route('comics.show', $comic) }}">datails</a>
+            <li>
+                {{ $comic->id }} - {{ $comic->title }} - <a href="{{ route('comics.show', $comic) }}">datails</a>
             </li>
         @endforeach
     </ul>
-    <a href="{{ route('home') }}">Home</a>
 @endsection
