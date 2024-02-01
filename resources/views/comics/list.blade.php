8955b5d80bf82d735f44abfa9c4d00f8cd48aa6c
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-01 18:41:43 +0100

    feat: layout condiviso

--- resources/views/comics/list.blade.php
+++ resources/views/comics/list.blade.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
+@section('main')
     <h1>Comics</h1>
     <ul>
         @foreach ($comics as $comic)
-            <li>{{ $comic->id }} - {{ $comic->title }} - <a href="{{ route('comics.show', $comic->id) }}">datails</a>
+            <li>{{ $comic->id }} - {{ $comic->title }} - <a href="{{ route('comics.show', $comic) }}">datails</a>
             </li>
         @endforeach
     </ul>
-</body>
-
-</html>
+@endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $comic->title }}</title>
-</head>
-
-<body>
+@section('main')
     <h1>{{ $comic->title }}</h1>
     <a href="{{ route('comics.index') }}">Back</a>
-</body>
-
-</html>
+@endsection
