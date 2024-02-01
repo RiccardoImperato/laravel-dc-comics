916e338ae3f6d8a7dc1cc70d291b19272b0b97e7
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-01 20:12:37 +0100

    feat: info comic

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,6 +1,16 @@
 @extends('layout.app')
 
 @section('main')
-    <h1>{{ $comic->title }}</h1>
+    <figure class="figure">
+        <img src="{{ $comic->thumb }}" class="rounded" alt="{{ $comic->title }}">
+    </figure>
+    <div>
+        <h3 class="my-3">{{ $comic->title }}</h3>
+        <p class="mb-3">{{ $comic->description }}</p>
+        <div>Serie: {{ $comic->series }}</div>
+        <div>Price: {{ $comic->price }}$</div>
+        <div>Sale date: {{ $comic->sale_date }}</div>
+        <div>Type: {{ $comic->type }}</div>
+    </div>
     <a href="{{ route('comics.index') }}">Back</a>
 @endsection
