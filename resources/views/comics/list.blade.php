73fc42a09d9fa832c88d645f27a76f43b78ee414
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-02 19:10:50 +0100

    fix. stile pagina lista

--- resources/views/comics/list.blade.php
+++ resources/views/comics/list.blade.php
@@ -5,14 +5,16 @@
     <a href="{{ route('home') }}">Home</a> - <a href="{{ route('comics.create') }}">Add comic</a>
     <ul class="list-unstyled mt-3">
         @foreach ($comics as $comic)
-            <li>
-                {{ $comic->id }} - {{ $comic->title }} -
-                <a href="{{ route('comics.show', $comic) }}">Details</a> -
-                <a href="{{ route('comics.edit', $comic) }}">Edit comic</a>
+            <li class="d-flex justify-content-between align-items-center">
+                <div>
+                    {{ $comic->id }} - {{ $comic->title }} -
+                    <a href="{{ route('comics.show', $comic) }}">Details</a> -
+                    <a href="{{ route('comics.edit', $comic) }}">Edit comic</a>
+                </div>
                 <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
                     @csrf
                     @method('DELETE')
-                    <input type="submit" value="Delete comic">
+                    <input class="btn btn-danger my-3" type="submit" value="Delete comic">
                 </form>
             </li>
         @endforeach
